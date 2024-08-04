192b3141885339ac5188f8d2d1d59f077cf99d3f
Timchinnn <nevarui62@gmail.com>
2024-08-04 16:10:54 +0300

    modified:   src/App.js 	new file:   src/components/Button/Button.css 	new file:   src/components/Button/Button.jsx 	new file:   src/components/Header/Header.css 	new file:   src/components/Header/Header.jsx

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import { useEffect } from "react";
 import "./App.css";
 // window.Telegram.Webapp.
 const tg = window.Telegram.WebApp;
+
 function App() {
   useEffect(() => {
     tg.ready();
   }, []);
-  const onClose = () => {
-    tg.close();
-  };
-  return (
-    <div className="App">
-      work <button onClick={onClose}>Закрыть</button>
-    </div>
-  );
+
+  return <div className="App">work</div>;
 }
 
 export default App;
