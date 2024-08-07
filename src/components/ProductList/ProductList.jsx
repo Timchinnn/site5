a5fdc839d49090a2bcf91ed2d0dc7557d4f3274a
Timchinnn <nevarui62@gmail.com>
2024-08-07 10:29:13 +0300

    modified:   src/components/ProductList/ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -21,34 +21,56 @@ const getTotalPrice = (items = []) => {
 
 const ProductList = () => {
   const [addedItems, setAddedItems] = useState([]);
+  const { tg, queryId } = useTelegram();
 
-  const { tg } = useTelegram();
+  const onSendData = useCallback(() => {
+    const data = {
+      products: addedItems,
+      totalPrice: getTotalPrice(addedItems),
+      queryId,
+    };
+    fetch("http://85.119.146.179:8000/web-data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }, [addedItems]);
+
+  useEffect(() => {
+    tg.onEvent("mainButtonClicked", onSendData);
+    return () => {
+      tg.offEvent("mainButtonClicked", onSendData);
+    };
+  }, [onSendData]);
 
   const onAdd = (product) => {
     const alreadyAdded = addedItems.find((item) => item.id === product.id);
     let newItems = [];
+
     if (alreadyAdded) {
       newItems = addedItems.filter((item) => item.id !== product.id);
     } else {
       newItems = [...addedItems, product];
     }
+
     setAddedItems(newItems);
+
     if (newItems.length === 0) {
       tg.MainButton.hide();
     } else {
       tg.MainButton.show();
-      tg.MainButton.setParams({ text: `Купить за ${getTotalPrice(newItems)}` });
+      tg.MainButton.setParams({
+        text: `Купить ${getTotalPrice(newItems)}`,
+      });
     }
   };
 
   return (
     <div className={"list"}>
       {products.map((item) => (
-        <ProductItem
-          product={item}
-          onAddd={onAddd}
-          className={"item"}
-        ></ProductItem>
+        <ProductItem product={item} onAdd={onAdd} className={"item"} />
       ))}
     </div>
   );
