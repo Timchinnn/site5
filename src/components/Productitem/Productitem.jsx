b450366c5b32691dfe3cf645fb6845230bd0d855
Timchinnn <nevarui62@gmail.com>
2024-08-07 10:18:38 +0300

    modified:   src/components/Productitem/Productitem.jsx

--- src/components/Productitem/Productitem.jsx
+++ src/components/Productitem/Productitem.jsx
@@ -6,14 +6,14 @@ const Productitem = (product, className, onAdd) => {
   const onAddHandler = () => {
     onAdd(product);
   };
+
   return (
     <div className={"product " + className}>
-      <div className={"img"}></div>
+      <div className={"img"} />
       <div className={"title"}>{product.title}</div>
       <div className={"description"}>{product.description}</div>
       <div className={"price"}>
         <span>
-          {" "}
           Стоимость: <b>{product.price}</b>
         </span>
       </div>
